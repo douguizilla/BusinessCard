c70d14e486a32bac727eb7b1592c1ccadc3cfcdc
douguizilla <douglasgommez@gmail.com>
2021-07-16 17:05:10 -0300

    create layout for fill the card

---
+++ app/src/main/java/com/odougle/businesscard/presentation/AddBusinessCardActivity.kt
@@ -0,0 +1,12 @@
+package com.odougle.businesscard.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.odougle.businesscard.R
+
+class AddBusinessCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_business_card)
+    }
+}
