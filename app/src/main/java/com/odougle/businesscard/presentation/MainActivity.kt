ffa8ae931ee01b2d1b73744c0a749e98beedff08
douguizilla <douglasgommez@gmail.com>
2021-07-16 15:34:46 -0300

    using ActivityMainBinding to inflate

--- app/src/main/java/com/odougle/businesscard/presentation/MainActivity.kt
+++ app/src/main/java/com/odougle/businesscard/presentation/MainActivity.kt
@@ -3,10 +3,12 @@ package com.odougle.businesscard.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.odougle.businesscard.R
+import com.odougle.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
