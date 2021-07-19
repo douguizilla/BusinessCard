7103729b4df92bd356dda0033315329fd0c4cb20
douguizilla <douglasgommez@gmail.com>
2021-07-19 10:43:23 -0300

    create method to fill the view with data

--- app/src/main/java/com/odougle/businesscard/presentation/MainActivity.kt
+++ app/src/main/java/com/odougle/businesscard/presentation/MainActivity.kt
@@ -3,13 +3,20 @@ package com.odougle.businesscard.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.odougle.businesscard.App
 import com.odougle.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val mainViewModel: MainViewModel by viewModels{
+        MainViewModelFactory((application as App).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        getAllBusinessCard()
         insertListener()
     }
 
@@ -18,5 +25,12 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun getAllBusinessCard(){
+        mainViewModel.getAll().observe(this, { businessCard ->
+
+
+        })
     }
 }
