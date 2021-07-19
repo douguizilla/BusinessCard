a8de1b7087fad2887817415350181fa60b94ff0e
douguizilla <douglasgommez@gmail.com>
2021-07-19 10:28:38 -0300

    set App.kt to start database and repository when the app starts

--- app/src/main/java/com/odougle/businesscard/App.kt
+++ app/src/main/java/com/odougle/businesscard/App.kt
@@ -1,6 +1,10 @@
 package com.odougle.businesscard
 
 import android.app.Application
+import com.odougle.businesscard.data.AppDatabase
+import com.odougle.businesscard.data.BusinessCardRepository
 
 class App: Application() {
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
